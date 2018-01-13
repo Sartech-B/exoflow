b27f227ac59ac6d794248355ed04b114328ac607
Sartech-B <sarthakbawal@gmail.com>
2018-01-13 22:59:38 +0400

    Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='exoflow',
-      version='0.1.2.3',
+      version='0.1.2.4',
       description='Python Module.',
       url='https://github.com/Sartech-B/exoflow',
       author='Sarthak Bawal',
